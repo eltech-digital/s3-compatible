120294d5238cad4bb9eb2680f3a563db8f405c91
Agus Saputra Sijabat <agussaputrapro@live.com>
2026-02-22 20:47:26 +0700

    chore: Include DB_PASSWORD in the Drizzle database connection URL.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     out: './drizzle',
     dialect: 'mysql',
     dbCredentials: {
-        url: `mysql://${process.env.DB_USER || 'root'}@${process.env.DB_HOST || '127.0.0.1'}:${process.env.DB_PORT || '3306'}/${process.env.DB_NAME || 's3'}`,
+        url: `mysql://${process.env.DB_USER || 'root'}:${process.env.DB_PASSWORD || ''}@${process.env.DB_HOST || '127.0.0.1'}:${process.env.DB_PORT || '3306'}/${process.env.DB_NAME || 's3'}`,
     },
 });
